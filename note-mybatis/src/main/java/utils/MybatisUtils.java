e105839f68f42e63ccc2da8bc92d3a6b6195e935
RandyForest <randy7100@outlook.com>
2019-10-23 15:19:06 +0800

    添加Mybatis缓存笔记

--- note-mybatis/src/main/java/utils/MybatisUtils.java
+++ note-mybatis/src/main/java/utils/MybatisUtils.java
@@ -10,26 +10,25 @@ import java.io.Reader;
 
 /**
  * 创建一个 SqlSession 实例
- * SqlSession 的实例在同配置项目中只需要一个
- *
+ * SqlSessionFactory 的实例在同配置项目中只需要一个
+ * <p>
  * Author: randy
  * Date: 2019/10/22 18:54
  */
 public class MybatisUtils {
-    private static SqlSession sqlSession;
+    private static SqlSessionFactory sqlSessionFactory;
 
     static {
         try {
             Reader resourceAsReader = Resources.getResourceAsReader("./SqlMapConfig.xml");
             SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(resourceAsReader);
-            sqlSession = sqlSessionFactory.openSession();
+            sqlSessionFactory = sqlSessionFactoryBuilder.build(resourceAsReader);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static SqlSession getSqlSession(){
-        return sqlSession;
+    public static SqlSession getSqlSession() {
+        return sqlSessionFactory.openSession();
     }
 }
--- note-mybatis/src/test/java/start/CacheTest.java
+++ note-mybatis/src/test/java/start/CacheTest.java
@@ -1,18 +1,37 @@
 package start;
 
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
 import utils.MybatisUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * 在同个SqlSession中，查询语句相同的sql会被缓存，
  * 但是一旦执行新增或更新或删除操作，缓存就会被清除
+ * <p>
+ * 一级缓存
+ * 本地的只在SqlSession对象中生效
+ * 默认开启
+ * test() 与 test2() 测试一级缓存
+ * <p>
+ * 二级缓存
+ * 全局的，所有使用该Mapper配置文件的程序都生效
+ * 需要手动开启
+ * Bean类需要实现Serializable接口
+  <p>
  * Author: randy
  * Date: 2019/10/22 23:01
  */
 public class CacheTest {
-    @Test
-    void test(){
+    /**
+     * 一级缓存
+     */  @Test
+    void test() {
         SqlSession sqlSession = MybatisUtils.getSqlSession();
 
         User user1 = sqlSession.selectOne("start.UserDao.query", 1);
@@ -26,8 +45,11 @@ public class CacheTest {
         sqlSession.close();
     }
 
+    /**
+     * 一级缓存被提交时清空
+     */
     @Test
-    void test2(){
+    void test2() {
         SqlSession sqlSession = MybatisUtils.getSqlSession();
 
         User user1 = sqlSession.selectOne("start.UserDao.query", 1);
@@ -42,6 +64,55 @@ public class CacheTest {
 
         sqlSession.commit();
         sqlSession.close();
+    }
 
+    /**
+     * 重新创建SqlSession后一级缓存无用
+     */
+    @Test
+    void test3(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        User user1 = sqlSession.selectOne("start.UserDao.query", 1);
+        System.out.println(user1);
+
+        sqlSession.commit();
+        sqlSession.close();
+
+
+        // 创建新SqlSession
+        SqlSession sqlSession2 = MybatisUtils.getSqlSession();
+
+        User user2 = sqlSession2.selectOne("start.UserDao.query", 1);
+        System.out.println(user2);
+
+        sqlSession2.commit();
+        sqlSession2.close();
+
+    }
+
+    /**
+     * 使用二级缓存配置
+     * 二级缓存不受SqlSession对象的影响
+     * @throws IOException
+     */
+    @Test
+    void test4() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("./SqlMapConfigCache.xml");
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        cache.User user = sqlSession.selectOne("cache.UserDao.query", 1);
+        System.out.println(user);
+        sqlSession.commit();
+        sqlSession.close();
+
+        // 创建新SqlSession
+        SqlSession sqlSession2 = sqlSessionFactory.openSession();
+        cache.User user2 = sqlSession2.selectOne("cache.UserDao.query", 1);
+        System.out.println(user2);
+        sqlSession.commit();
+        sqlSession2.close();
     }
 }
